01ee1c655d728cb2a80b0331e51bd0afb29f974b
Kate Lueders <133680405+kelueders@users.noreply.github.com>
2024-10-07 16:15:09 +0000

    Complete recursion

--- Start/Recursion/recursion_start.py
+++ Start/Recursion/recursion_start.py
@@ -1,14 +1,27 @@
 # Example file for Programming Foundations: Algorithms by Joe Marini
 # Using recursion to implement power and factorial functions
 
+'''
+Recursion
+- functions need to have a breaking condition
+- each time the function is called the old arguments are saved
+- this is called the call stack
+'''
+
 # 2^4 = 2*2*2*2 = 16
 def power(num, pwr):
-    pass
+    if pwr == 0:
+        return 1
+    else:
+        return (num * power(num, pwr-1))
 
 # 5! = 5*4*3*2*1
 # Special case: 0! is 1, because... math
 def factorial(num):
-    pass
+    if num == 0:
+        return 1
+    else:
+        return num * factorial(num - 1)
 
 
 print(f"5 to the power of 3 is {power(5, 3)}")
